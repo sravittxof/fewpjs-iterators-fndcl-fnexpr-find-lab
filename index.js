3741b409e0845284c50312b1ca1707991d3d0074
sravittxof <travis.fox90@gmail.come>
2021-04-26 21:07:49 -0400

    commit tree 2e9f00a80573a114e6c9fc5c93a03d59501e6942

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+  function superbowlWin(record) {
+    let winningSeason = record.find(function(game){
+        return game['result'] === "W";
+    })
+    if (winningSeason){
+        return winningSeason['year'];
+    }
+  }
